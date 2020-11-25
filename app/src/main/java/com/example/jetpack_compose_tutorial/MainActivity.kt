6a98e7c786cd9447db06999d6b6c56d1bd86448f
Mark Leung <mark.leung89@gmail.com>
2020-11-25 13:11:19 +1100

    Add Login Screen

--- app/src/main/java/com/example/jetpack_compose_tutorial/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_tutorial/MainActivity.kt
@@ -6,8 +6,17 @@ import androidx.compose.foundation.Text
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
+import androidx.compose.foundation.Box
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.TextField
+import androidx.compose.ui.draw.drawShadow
 import com.example.jetpack_compose_tutorial.ui.JetpackComposeTutorialTheme
 
 class MainActivity : AppCompatActivity() {
@@ -17,7 +26,7 @@ class MainActivity : AppCompatActivity() {
             JetpackComposeTutorialTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    LoginForm()
                 }
             }
         }
@@ -31,13 +40,51 @@ class MainActivity : AppCompatActivity() {
     @Composable
     fun DefaultPreview() {
         JetpackComposeTutorialTheme {
-            Greeting("from Mark")
+            //Greeting("from Mark")
+            //BoxLayout()
+            LoginFormPreview()
         }
     }
 
     @Composable
     fun BoxLayout() {
-        Box(backgroundColor = Color.Blue, modifier = Modifier.size(200,dp))
+        Stack(alignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+            Box(backgroundColor = Color.Blue, modifier = Modifier.size(200.dp), gravity = Alignment.TopEnd ) {
+                Box(
+                        backgroundColor = Color.Cyan,
+                        modifier = Modifier.size(25.dp)
+                )
+            }
+            Box(backgroundColor = Color.Red, modifier = Modifier.size(100.dp), gravity = Alignment.BottomStart) {
+                Box(
+                        backgroundColor = Color.Magenta,
+                        modifier = Modifier.size(25.dp)
+                )
+            }
+        }
+    }
+
+    @Composable
+    fun LoginForm() {
+        Box(gravity = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                val modifier = Modifier.padding(vertical = 4.dp)
+
+
+                TextField(value = "", onValueChange = {}, modifier = modifier)
+                TextField(value = "", onValueChange = {}, modifier = modifier.then(Modifier.drawShadow(elevation = 3.dp)) )
+                Button(onClick = {}, modifier = modifier) {
+                    Text("Login")
+                }
+            }
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun LoginFormPreview() {
+        LoginForm()
     }
 }
+
 

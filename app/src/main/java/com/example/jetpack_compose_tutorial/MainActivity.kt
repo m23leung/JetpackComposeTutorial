8f70a4f4bfe65416739cdf2972eedd36408d2cdb
Mark Leung <mark.leung89@gmail.com>
2020-11-25 12:11:01 +1100

    Second commit

---
+++ app/src/main/java/com/example/jetpack_compose_tutorial/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.jetpack_compose_tutorial
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
+import com.example.jetpack_compose_tutorial.ui.JetpackComposeTutorialTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeTutorialTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+    @Composable
+    fun Greeting(name: String) {
+        Text(text = "Hello $name!")
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        JetpackComposeTutorialTheme {
+            Greeting("from Mark")
+        }
+    }
+
+    @Composable
+    fun BoxLayout() {
+        Box(backgroundColor = Color.Blue, modifier = Modifier.size(200,dp))
+    }
+}
+
